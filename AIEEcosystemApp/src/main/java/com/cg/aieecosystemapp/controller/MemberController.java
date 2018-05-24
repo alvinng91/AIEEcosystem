2c9b126c76fbf51bd83e661b1f4b96cfa0f36b55
nicchin91 <nicholas.chin@capgemini.com>
2018-05-24 12:53:14 +0800

    added http response

--- AIEEcosystemApp/src/main/java/com/cg/aieecosystemapp/controller/MemberController.java
+++ AIEEcosystemApp/src/main/java/com/cg/aieecosystemapp/controller/MemberController.java
@@ -1,6 +1,8 @@
 package com.cg.aieecosystemapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,40 +13,70 @@ import com.cg.aieecosystemapp.service.MemberService;
 
 @RestController
 @RequestMapping(path = "/api/member")
-public class MemberController
-{
-    @Autowired
-    private MemberService service;
+public class MemberController {
+	@Autowired
+	private MemberService service;
 
-    @RequestMapping(method = RequestMethod.POST)
-    public Member createMember(String firstName, String lastName, String position, String email, int tier,
-	    String password)
-    {
-	return service.createMember(firstName, lastName, position, email, tier, password);
-    }
-    
-    @RequestMapping(method = RequestMethod.GET, value = "/authenticate")
-    public Member authMember(String email, String password)
-    {
-	return service.authenticateMember(email, password);
-    }
-    
-    @RequestMapping(method = RequestMethod.GET)
-    public Member getMember(String email)
-    {
-	return service.getExistingMember(email);
-    }
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<?> createMember(String firstName, String lastName, String position, String email, int tier,
+			String password) {
+		
+		try{
+			Member m = service.createMember(firstName, lastName, position, email, tier, password);
+			return new ResponseEntity<>(m, HttpStatus.CREATED);
+		}
+		catch(Exception e) {
+			return new ResponseEntity<>("create fail" ,HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+	}
 
-    @RequestMapping(method = RequestMethod.PUT)
-    public Member updateMember(String email, int tier)
-    {
-	return service.updateMemberTier(email, tier);
-    }
-   
-    @RequestMapping(method = RequestMethod.DELETE)
-    public Member deleteExistingMember(String email)
-    {
-	return service.deleteExistingMember(email);
-    }
-    
+	@RequestMapping(method = RequestMethod.GET, value = "/authenticate")
+	public ResponseEntity<?> authMember(String email, String password) {
+		
+		try{
+			Member m = service.authenticateMember(email, password);
+			return new ResponseEntity<>(m, HttpStatus.OK);
+		}
+		catch(Exception e) {
+			return new ResponseEntity<>("auth member fail" ,HttpStatus.FORBIDDEN);
+		}
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<?> getMember(String email) {
+		
+		try{
+			Member m = service.getExistingMember(email);
+			return new ResponseEntity<>(m, HttpStatus.FOUND);
+		}
+		catch(Exception e) {
+			return new ResponseEntity<>("get member fail",HttpStatus.NOT_FOUND);
+		}
+		
+	}
+
+	@RequestMapping(method = RequestMethod.PUT)
+	public ResponseEntity<?> updateMember(String email, int tier) {
+		
+		try{
+			Member m = service.updateMemberTier(email, tier);
+			return new ResponseEntity<>(m, HttpStatus.OK);
+		}
+		catch(Exception e) {
+			return new ResponseEntity<>("update fail",HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@RequestMapping(method = RequestMethod.DELETE)
+	public ResponseEntity<?> deleteExistingMember(String email) {
+		try{
+			Member m = service.deleteExistingMember(email);
+			return new ResponseEntity<>(m, HttpStatus.OK);
+		}
+		catch(Exception e) {
+			return new ResponseEntity<>("delete fail",HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
 }
